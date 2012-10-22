39e94496ff10817dec17657f1fc43cb3a8533226
Rahul Madan <rahulsachint@gmail.com>
2012-10-22 04:40:18 -0400

    user login completed

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+
+class PagesController < ApplicationController
+  def home
+    @title = "Home"
+
+    if signed_in?
+      
+    end
+  end
+
+  def contact
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About"
+  end
+  
+  def help
+    @title = "Help"
+  end
+  
+end
---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,16 @@
+module ApplicationHelper
+  
+def title
+  baseTitle = "Sample App"
+  if @title.nil?
+      baseTitle
+  else 
+      "#{baseTitle} | #{@title}"
+  end     
+end
+
+def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+end
+
+end
